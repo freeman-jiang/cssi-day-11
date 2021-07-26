62f7c2eedb15cded0bc1d0f614e90ca1536cb4fd
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-07-26 17:37:49 +0000

    auth working

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,4 +1,22 @@
-const signInButton = document.querySelector(".button")
-signInButton.addEventListener("click", () => {
-    console.log("clicked!")
-})
+console.log('hello');
+
+const signIn = async () => {
+    
+    try {
+        console.log('clicked');
+        const provider = new firebase.auth.GoogleAuthProvider();
+        const res = await firebase.auth().signInWithPopup(provider);
+        const user = res.user;
+        console.log('login success', user);
+
+        // firebase.auth.signInWithPopup(provider).then(result => {
+        // const user = result.user
+        // console.log('login success', user)
+        // })
+
+        // window.location = "writeNote.html"
+
+    } catch (err) {
+        console.error('login failed', err);
+    }
+};
