522c1b88f6c403633e95967a5f79f4f40f6ec182
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-07-26 18:54:44 +0000

    Easy and Medium stretch goals

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -5,6 +5,12 @@ window.onload = () => {
         if (user) {
             console.log('logged in as ', user.displayName)
             googleUser = user
+            const userGreeting = document.querySelector('#userGreeting')
+            const userInfo = document.querySelector('#userInfo')
+            userGreeting.innerHTML = `Hello ${user.displayName}`
+            userInfo.innerHTML = `Write what should not be forgotten`
+            
+            
         } else {
             console.log('not logged in')
         }
@@ -15,12 +21,19 @@ const createNote = async () => {
     // GET INPUTS
     const title = document.querySelector('#noteTitle').value
     const text = document.querySelector('#noteText').value
+    const date = new Date()
+    const label = document.querySelector('#label').value
+
+    console.log(date)
 
     // WRITE TO DATABASE
     try {
         firebase.database().ref(`users/${googleUser.uid}`).push({
+            created: date.toString(),
             title,
-            text
+            text,
+            label
+            
         })
         console.log('database write successful')
         document.querySelector('#noteTitle').value = ""
@@ -30,7 +43,6 @@ const createNote = async () => {
     }
     catch (err) {
         console.error('error! ', err);
-
     }
 
 
